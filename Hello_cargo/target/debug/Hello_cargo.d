836f4035aea4198572e9c6ba0f9f90f15869bf24
bruce <your_email@example.com>
2020-05-04 21:02:35 +0800

    Hello_cargo-builded

---
+++ Hello_cargo/target/debug/Hello_cargo.d
@@ -0,0 +1,1 @@
+/home/zhang/Github/Rust_Practice/Hello_cargo/target/debug/Hello_cargo: /home/zhang/Github/Rust_Practice/Hello_cargo/src/main.rs
---
+++ Hello_cargo/target/debug/deps/Hello_cargo-541a54639c8e700a.d
@@ -0,0 +1,5 @@
+/home/zhang/Github/Rust_Practice/Hello_cargo/target/debug/deps/Hello_cargo-541a54639c8e700a: src/main.rs
+
+/home/zhang/Github/Rust_Practice/Hello_cargo/target/debug/deps/Hello_cargo-541a54639c8e700a.d: src/main.rs
+
+src/main.rs:
